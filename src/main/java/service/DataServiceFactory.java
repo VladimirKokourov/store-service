c85fb1bba66ae001f01474457a3b7a9424ec86b2
vkoko <@vkokourov>
2023-10-06 14:26:42 +0700

    Create DataServiceFactory

---
+++ src/main/java/service/DataServiceFactory.java
@@ -0,0 +1,32 @@
+package service;
+
+import dao.Repository;
+import exception.UnknownCommandException;
+import pojo.input.search.SearchRequestRoot;
+import pojo.input.search.criteria.Criteria;
+import pojo.input.stat.StatRequestRoot;
+import util.JsonUtil;
+
+import java.util.List;
+
+public class DataServiceFactory {
+
+    public static final String SEARCH = "search";
+    public static final String STAT = "stat";
+
+    public static DataService getDataService(String command, String inputFilePath,
+                                             String outputFilePath, Repository repository) {
+        switch (command) {
+            case SEARCH :
+                SearchRequestRoot searchRequestRoot = (SearchRequestRoot) JsonUtil.readJson(inputFilePath);
+                List<Criteria> criterias = searchRequestRoot.getCriterias();
+                return new CriteriasDataService(criterias, repository, outputFilePath);
+            case STAT:
+                StatRequestRoot statRequestRoot = (StatRequestRoot) JsonUtil.readJson(inputFilePath);
+                return new StatCustomersDataService(statRequestRoot.getStartDate(), statRequestRoot.getEndDate(),
+                        repository, outputFilePath);
+            default:
+                throw new UnknownCommandException("Неизвестная команда: " + command);
+        }
+    }
+}
