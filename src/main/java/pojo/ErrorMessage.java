2273cffab74d4ccbdf9dbd300f90cc427fd54934
vkoko <@vkokourov>
2023-10-06 20:19:07 +0700

    Create ErrorMessage

---
+++ src/main/java/pojo/ErrorMessage.java
@@ -0,0 +1,16 @@
+package pojo;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Getter
+@Setter
+public class ErrorMessage {
+
+    private String type;
+    private String message;
+}
