dc26050fed51510e74325e1ffbaae6d5db8eb380
vkoko <@vkokourov>
2023-10-06 21:34:47 +0700

    Refactor

--- src/main/java/pojo/output/stat/CustomerPurchaseStat.java
+++ src/main/java/pojo/output/stat/CustomerPurchaseStat.java
@@ -3,7 +3,6 @@ package pojo.output.stat;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
-import lombok.Setter;
 
 import java.math.BigDecimal;
 import java.util.List;
